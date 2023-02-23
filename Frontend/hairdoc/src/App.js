81bdddd46cc7fc34c761f1a822ecbd573c5055eb
Ankeshkewat <ankeshkewat966@gmail.com>
2023-02-24 01:36:07 +0530

    Home page added

--- Frontend/hairdoc/src/App.js
+++ Frontend/hairdoc/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import CaptionCarousel from './Components/Carousal';
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
+       <Footer/>
     </div>
+
   );
 }
 
