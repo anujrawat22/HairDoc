675052ea7fe256520a19268f2334ccee90955dc8
Ankeshkewat <ankeshkewat966@gmail.com>
2023-02-24 13:18:44 +0530

    Carousal added

--- Frontend/hairdoc/src/Components/Carousal.jsx
+++ Frontend/hairdoc/src/Components/Carousal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactNode } from 'react';
+import { ReactNode,useState } from 'react';
 import {
   Box,
   IconButton,
@@ -25,27 +25,27 @@ const settings = {
 };
 
   const CaptionCarousel= ()=> {
-  const [slider, setSlider] = React.useState<Slider | null>(null);
+  const [slider,setSlider]=useState(null)
   const top = useBreakpointValue({ base: '90%', md: '50%' });
   const side = useBreakpointValue({ base: '30%', md: '40px' });
 
   const cards = [
     {
-      title: 'Design Projects 1',
+      title: 'Image 1',
       text:
         "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
         'https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
     },
     {
-      title: 'Design Projects 2',
+      title: 'Image 2',
       text:
         "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
         'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80',
     },
     {
-      title: 'Design Projects 3',
+      title: 'Image 3',
       text:
         "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
       image:
@@ -60,7 +60,7 @@ const settings = {
       height={'600px'}
       width={'full'}
       overflow={'hidden'}>
-      {/* CSS files for react-slick */}
+
       <link
         rel="stylesheet"
         type="text/css"
@@ -72,7 +72,7 @@ const settings = {
         type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
       />
-      {/* Left Icon */}
+
       <IconButton
         aria-label="left-arrow"
         variant="ghost"
@@ -84,7 +84,7 @@ const settings = {
         onClick={() => slider?.slickPrev()}>
         <BiLeftArrowAlt size="40px" />
       </IconButton>
-      {/* Right Icon */}
+
       <IconButton
         aria-label="right-arrow"
         variant="ghost"
@@ -96,7 +96,7 @@ const settings = {
         onClick={() => slider?.slickNext()}>
         <BiRightArrowAlt size="40px" />
       </IconButton>
-      {/* Slider */}
+
       <Slider {...settings} ref={(slider) => setSlider(slider)}>
         {cards.map((card, index) => (
           <Box
--- Frontend/hairdoc/src/pages/Home.jsx
+++ Frontend/hairdoc/src/pages/Home.jsx
@@ -6,6 +6,7 @@ function Home(){
     return (
         <div>
             <Navbar/>
+            <CaptionCarousel />
             <Footer />
         </div>
     )
