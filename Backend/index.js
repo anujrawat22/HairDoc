2dc1ad145eec73f7e60696ab351efbc0fd50f132
EktaDhawal <51295793+dekta@users.noreply.github.com>
2023-02-24 15:10:54 +0530

    logout done

--- Backend/index.js
+++ Backend/index.js
@@ -1,61 +1,69 @@
-
-const express = require('express');
-const cors = require('cors')
-const jwt=require('jsonwebtoken')
-require('dotenv').config()
-var cookieParser = require('cookie-parser')
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+var cookieParser = require("cookie-parser");
 
 const app = express();
-const {passport}=require('./config/google-oauth')
+const { passport } = require("./config/google-oauth");
 
-const {authenticate} = require("./middlewares/authentication")
+const { authenticate } = require("./middlewares/authentication");
+const { checkRouter } = require("./routes/checkAuth.route");
 
-app.use(express.json())
-app.use(cors({
-    origin: '*'
-}))
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*"
+  })
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
+app.use("/check", authenticate, checkRouter);
 
-const { connection } = require('./config/db.js')
+const { connection } = require("./config/db.js");
 
-const {UserRouter}=require('./routes/user_signup.router')
-const {validate}=require('./middlewares/validate')
-const {validate_google_oauth}=require('./middlewares/validate-google-auth')
+const { UserRouter } = require("./routes/user_signup.router");
+const { validate } = require("./middlewares/validate");
+const { validate_google_oauth } = require("./middlewares/validate-google-auth");
 
-app.post('/signup',validate,UserRouter)
-app.post('/login',UserRouter)
-app.post('/verify',UserRouter)
-app.get('/',(req,res)=>{
-    res.send("This is base router")
-})
+app.post("/signup", validate, UserRouter);
+app.post("/login", UserRouter);
+app.post("/verify", UserRouter);
+app.get("/", (req, res) => {
+  res.send("This is base router");
+});
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile','email'] }));
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login',session:false}),
-  function({user}, res) {
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+    session: false
+  }),
+  function ({ user }, res) {
     // Successful authentication, redirect home.
-    const token = jwt.sign({ userid: user._id, email:user.email,isAdmin:user.isAdmin }, process.env.password, { expiresIn: '5 days' })
-    console.log(user)
-    res.status(201).send({'msg':'Login succesfull','token':token})
+    const token = jwt.sign(
+      { userid: user._id, email: user.email, isAdmin: user.isAdmin },
+      process.env.password,
+      { expiresIn: "5 days" }
+    );
+    console.log(user);
+    res.status(201).send({ msg: "Login succesfull", token: token });
     // res.redirect('/');
-  });
-
-
-
+  }
+);
 
 app.listen(8080, async () => {
-    try {
-        await connection
-        console.log('connected to db')
-        console.log('listening in port 8080')
-    }
-    catch (err) {
-        console.log(err)
-        console.log('error in connecting')
-    }
-})
+  try {
+    await connection;
+    console.log("connected to db");
+    console.log("listening in port 8080");
+  } catch (err) {
+    console.log(err);
+    console.log("error in connecting");
+  }
+});
