eed7b3f19d56ec5b5829029b78276a5136db3fb9
anujrawat22 <anuj22rawat20@gmail.com>
2023-05-07 22:14:28 +0530

    booking model

--- Backend/controller/booking.js
+++ Backend/controller/booking.js
@@ -25,20 +25,27 @@ exports.createBooking = async (req, res) => {
   } catch (error) {}
 };
 
-exports.updateBooking = async (req, res) => {
-  try {
-    const {id} = req.params
-    const {UserId} = req.body;
-    const Booking = await BookingModel.findOne({UserId})
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ Error: "Server Error" });
-  }
-};
+// exports.updateBooking = async (req, res) => {
+//   try {
+//     const {id} = req.params
+//     const {UserId} = req.body;
+//     const Booking = await BookingModel.findOne({UserId})
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send({ Error: "Server Error" });
+//   }
+// };
 
 exports.deleteBooking = async (req, res) => {
   try {
-  
+      const {id} = req.params
+      const {UserId} = req.body;
+      const booking = await BookingModel.find({UserId})
+      if(booking){
+        await BookingModel.findByIdAndDelete(id)
+        return res.status(201).send({msg : "Booking deleted sucessfully"})
+      }
+      res.status(401).send({msg : "Unauthorized"})
   } catch (error) {
     console.log(error);
     res.status(500).send({ Error: "Server Error" });
